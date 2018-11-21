7e53dc274d775017f5804e91a775c20359321f07
vsofat <vsofat00@stuy.edu>
2018-11-21 18:22:06 -0500

    constructor added

---
+++ MyString.java
@@ -0,0 +1,12 @@
+public class MyString implements CharSequence,Comparable<CharSequence>{
+  private char[] data;
+  public MyString(CharSequence s){
+    data = new char[s.length()];
+    for (int i = 0; i < s.length(); i++){
+      data[i] = s.get(i);
+    }
+  }
+
+
+
+  }
